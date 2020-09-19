d17585172bb47ea520d4b5c2f5a65db4b3582cd1
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-09-19 01:45:02 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,10 @@
 # Write your code here
 
+magic_exit_number = 7
 count = 0
 
-while count < 3 do
+while count < 10 do
+  break if count == magic_exit_number
   puts "I am the #{count}, I love to count!"
   count = count + 1
 end
